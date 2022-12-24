7128db8b24a4865f7fde7de9bbe7b64aee9fc6a8
hassan <muhammadhassanlodhi123@gmail.com>
2022-12-25 01:24:30 +0500

    Final Commit

--- controllers/products.js
+++ controllers/products.js
@@ -28,16 +28,16 @@ const getAllProducts = async (req, res) => {
 
 
     let page = Number(req.query.page) || 1
-    let limit = Number(req.query.limit) || 3;
+    let limit = Number(req.query.limit) || 10;
 
     let skip = (page - 1) * limit;
     apiData = apiData.skip(skip).limit(limit)
 
     console.log(queryObject);
 
-    const myData = await apiData
+    const Products = await apiData
     res.status(200).json({
-        myData,nbHits:myData.length
+        Products,nbHits:Products.length
     });
 };
 
