a1f6fa5c3f16b6d8973835c61c4bf365d262b179
hassan <muhammadhassanlodhi123@gmail.com>
2022-12-25 01:15:06 +0500

    Rest Api Completed

--- controllers/products.js
+++ controllers/products.js
@@ -4,21 +4,46 @@ const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
 
-    const { company } = req.query;
-    const queryObject = {}
+    const { company, name, featured, sort, select } = req.query;
+    const queryObject = {};
 
     if (company) {
         queryObject.company = company;
-        console.log(queryObject);
+    };
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" };
+    };
+    if (featured) {
+        queryObject.featured = featured;
+    }
+    let apiData = Product.find(queryObject);
+    if (sort) {
+        let sortFix = sort.replace(",", " ");
+        apiData = apiData.sort(sortFix);
+    }
+    if (select) {
+        let selectFix = select.split(",").join(" ");
+        apiData = apiData.select(selectFix);
     }
 
-    const myData = await Product.find(req.query)
+
+    let page = Number(req.query.page) || 1
+    let limit = Number(req.query.limit) || 3;
+
+    let skip = (page - 1) * limit;
+    apiData = apiData.skip(skip).limit(limit)
+
+    console.log(queryObject);
+
+    const myData = await apiData
     res.status(200).json({
-        myData
+        myData,nbHits:myData.length
     });
 };
+
+
 const getAllProductsTesting = async (req, res) => {
-    const myData = await Product.find(req.query)
+    const myData = await Product.find(req.query).skip(2);
     res.status(200).json({
         myData
     });
